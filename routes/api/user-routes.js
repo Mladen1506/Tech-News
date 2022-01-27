c5748f0e40a2bbb6d7f3c5741af46c5e31c875fa
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-01-26 19:42:55 -0500

    CRUD done

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -33,8 +33,22 @@ router.get('/:id', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 router.post('/login', (req, res) => {
-    // expects {email: 'maka@gmail.com', password: 'password1234'}
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     User.findOne({
         where: {
             email: req.body.email
@@ -44,8 +58,9 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'No user with that email address!' });
             return;
         }
-        // Verify user
+
         const validPassword = dbUserData.checkPassword(req.body.password);
+
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
